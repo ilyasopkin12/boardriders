757c5b4463445b74b4ef56bb938e578f87287922
Ilya <steamistl@gmail.com>
2025-12-11 18:46:02 +0400

    адаптив под 414px

--- src/widgets/footer/footerHeader/footerHeader.tsx
+++ src/widgets/footer/footerHeader/footerHeader.tsx
@@ -1,8 +1,14 @@
 import styles from "./footerHeader.module.scss";
-import whiteLogo from "../../../shared/assets/images/whiteLogo.svg";
-import whiteLogo2 from "../../../shared/assets/images/whiteLogo2.svg";
-import emailGray from "../../../shared/assets/images/mailgrayIcon.svg"
-import sendWhite from "../../../shared/assets/images/sendArrowRight.svg"
+import whiteLogo from "@shared/assets/images/whiteLogo.svg";
+import whiteLogo2 from "@shared/assets/images/whiteLogo2.svg";
+import emailGray from "@shared/assets/images/mailgrayIcon.svg"
+import sendWhite from "@shared/assets/images/sendArrowRight.svg"
+import vkIcon from "@shared/assets/images/vkIconMobile.svg";
+import instagramIcon from "@shared/assets/images/instagramIconMobile.svg";
+import pictureFooter1 from "@shared/assets/images/pictureFooter1.png";
+import pictureFooter2 from "@shared/assets/images/pictureFooter2.png";
+import pictureFooter3 from "@shared/assets/images/pictureFooter3.png";
+import pictureFooter4 from "@shared/assets/images/pictureFooter4.png";
 export const FooterHeader = () => {
   return (
     <div className={styles.footerHeader}>
@@ -26,6 +32,26 @@ export const FooterHeader = () => {
             <img src={sendWhite} alt="send-icon" />
           </button>
         </label>
+      </div>
+      <div className={styles.footerHeader__mobileSubscribe}>
+        <div className={styles.footerHeader__mobileSubscribeTop}>
+          <p className={styles.footerHeader__mobileSubscribeTitle}>Подписывайся на нас</p>
+          <p className={styles.footerHeader__mobileSubscribeSocial}>@boardriders</p>
+          <div className={styles.footerHeader__mobileSubscribeTopSocial}>
+            <img src={vkIcon} alt="vk-icon" />
+            <img src={instagramIcon} alt="instagram-icon" />
+          </div>
+        </div>
+        <div className={styles.footerHeader__mobileSubscribeBottom}>
+          <div className={styles.footerHeader__mobileSubscribeBottomItem}>
+            <img src={pictureFooter1} alt="picture-footer-1" />
+            <img src={pictureFooter2} alt="picture-footer-2" />
+          </div>
+          <div className={styles.footerHeader__mobileSubscribeBottomItem}>
+            <img src={pictureFooter3} alt="picture-footer-3" />
+            <img src={pictureFooter4} alt="picture-footer-4" />
+          </div>
+        </div>
       </div>
     </div>
   );
